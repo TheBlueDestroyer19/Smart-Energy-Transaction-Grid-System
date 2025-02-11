58288cd8d00aa74a7fc49ac275a95d373d5ecfe9
salilphanse <158063144+TheBlueDestroyer19@users.noreply.github.com>
2025-02-12 00:23:36 +0530

    Update sortsellerbuyerpairs.c

--- sortsellerbuyerpairs.c
+++ sortsellerbuyerpairs.c
@@ -1,135 +1,113 @@
-#include "template.h"
 #include<stdlib.h>
 #include<stdio.h>
-Pair* MergeSort2(Pair* lptr);
-Pair* Merge2(Pair* list1, Pair* list2);
-Pair* Divide2(Pair* lptr);
+#include "template.h"
 
-Pair* Merge2(Pair* list1, Pair* list2) {
-    if (list1 == NULL) return list2;
-    if (list2 == NULL) return list1;
+Pair* MergePairs(Pair* list1, Pair* list2) {
+  Pair *retval=NULL,*ptr1=list1,*ptr2=list2,*tail=NULL; 
 
-    Pair* result = NULL;
-    Pair* tail = NULL;
-
-    Pair* ptr1 = list1;
-    Pair* ptr2 = list2;
-
-    if (ptr1->count > ptr2->count) {
-        result = tail = ptr1;
-        ptr1 = ptr1->next;
-    } else {
-        result = tail = ptr2;
-        ptr2 = ptr2->next;
+  if(list1==NULL) retval=list2;
+  else if(list2==NULL) retval=list1;
+  else {
+    if(ptr1->count>ptr2->count) {
+      retval=tail=ptr1;
+      ptr1=ptr1->next;
+    }
+    else {
+      retval=tail=ptr2;
+      ptr2=ptr2->next;
     }
 
-    while (ptr1 != NULL && ptr2 != NULL) {
-        if (ptr1->count > ptr2->count) {
-            tail->next = ptr1;
-            ptr1 = ptr1->next;
-        } else {
-            tail->next = ptr2;
-            ptr2 = ptr2->next;
-        }
-        tail = tail->next;
+    while (ptr1!=NULL && ptr2!=NULL) {
+      if (ptr1->count>ptr2->count) {
+        tail->next=ptr1;
+        ptr1=ptr1->next;
+      }
+      else {
+        tail->next=ptr2;
+        ptr2=ptr2->next;
+      }
+      tail=tail->next;
     }
 
-    if (ptr1 != NULL) {
-        tail->next = ptr1;
-    } else {
-        tail->next = ptr2;
-    }
-
-    return result;
+    if(ptr1!=NULL) 
+        tail->next=ptr1;
+    else 
+        tail->next=ptr2;
+  }
+  return retval;
 }
 
-Pair* Divide2(Pair* lptr) {
-    if (lptr == NULL || lptr->next == NULL) {
-        return NULL;
+Pair* DividePairs(Pair* lptr) {
+  Pair *fast,*slow,*nptr=NULL;
+  fast=slow=lptr;
+  if(lptr==NULL||lptr->next==NULL) nptr=NULL;
+  else {
+    fast=slow->next;
+    while (fast!=NULL && fast->next!=NULL) {
+      if(slow!=NULL) slow=slow->next;
+      fast=fast->next->next;
     }
 
-    Pair* slow = lptr;
-    Pair* fast = lptr->next;
-
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    Pair* nptr = slow->next;
+    nptr=slow->next;
     slow->next = NULL;
-    return nptr;
+  }
+  return nptr;
 }
 
-Pair* MergeSort2(Pair* lptr) {
-    if (lptr == NULL || lptr->next == NULL) {
-        return lptr;
+Pair* MergeSortPairs(Pair* lptr) {
+  Pair *retval,*nptr;
+  if (lptr==NULL || lptr->next==NULL) retval=lptr;
+  else {
+    nptr=DividePairs(lptr);
+    lptr=MergeSortPairs(lptr);
+    nptr=MergeSortPairs(nptr);
+    retval=MergePairs(lptr,nptr);
+  }
+  return retval;
+}
+Pair* sort_pairs(TransNode* mainhead, Pair* pairhead) {
+  int found;
+  TransNode* temp=mainhead;
+  Pair *pairs=NULL, *pptr;
+  if(temp!=NULL) {
+    while(temp!=NULL) {
+      if(temp->entry.flag==0) {
+        temp->entry.flag=1;
+        pairs=pairhead; found=0;
+        while(pairs!=NULL && pairs->next!=NULL && !found) {
+          if(temp->entry.sellerID==pairs->sellerID && temp->entry.buyerID==pairs->buyerID) found=1;
+          else pairs=pairs->next;
+        }  
+      
+        if(found) {
+          pairs->count++;
+        } 
+        else {
+          pptr=(Pair*)malloc(sizeof(Pair));
+          pptr->next=NULL;
+          if(pairhead==NULL) pairhead=pptr;
+          else pairs->next=pptr;
+          pairs=pptr;
+          pptr->count=1;
+          pptr->flag=0;
+          pptr->buyerID=temp->entry.buyerID;
+          pptr->sellerID=temp->entry.sellerID;
+        }
+      }
+      temp=temp->next;
     }
-
-    Pair* nptr = Divide2(lptr);
-    lptr = MergeSort2(lptr);
-    nptr = MergeSort2(nptr);
-    return Merge2(lptr, nptr);
-}
-void sort_pairs(TransNode* mainhead)
-{
     
-    TransNode* temp=mainhead;
-    if(temp!=NULL)
-    {
-    Pair* pairs=(Pair*)malloc(sizeof(Pair));
-    Pair* pairhead=pairs;
-    pairs->next=NULL;
-    
-    while(temp!=NULL)
-    {   
-        if(temp->entry.flag!=1)
-        {
-         pairs->count=1;
-         pairs->buyerID=temp->entry.buyerID;
-         pairs->sellerID=temp->entry.sellerID;
-        if(temp->next!=NULL)
-        {
-        TransNode* temp2=temp->next;
-        while(temp2!=NULL)
-        {
-            if(pairs->buyerID==temp2->entry.buyerID && pairs->sellerID==temp2->entry.sellerID)
-            {
-                pairs->count+=1;
-                temp2->entry.flag=1;
-            
-            }
-            temp2=temp2->next;
-        }
-        }
-        }
-        temp=temp->next;
-        
-        if(temp!=NULL&&temp->entry.flag!=1)
-        {Pair* ptr=(Pair*)malloc(sizeof(Pair));
-            pairs->next=ptr;
-            pairs=ptr;
-            pairs->next=NULL;
-        }
-        }
-    
-    pairhead=MergeSort2(pairhead);
+    pairhead=MergeSortPairs(pairhead);
     printf("Seller/Buyer pairs in decreasing order of the number of transactions between them are \n");
-    pairs=pairhead;
-    while(pairs!=NULL)
-    {   
-        printf("Seller ID:%d\n",pairs->sellerID);
-        printf("Buyer ID:%d\n",pairs->buyerID);
-        
-        printf("Count:%d\n\n",pairs->count);
-        pairs=pairs->next;
-    }
     
-    printf("Cleaning up the trash...done\n");
-    while(pairhead!=NULL)
-    {   Pair* ptr=pairhead;
-        pairhead=pairhead->next;
-        free(ptr);
+    pairs=pairhead;
+    while(pairs!=NULL) {   
+      printf("Seller ID:%d\n",pairs->sellerID);
+      printf("Buyer ID:%d\n",pairs->buyerID);
+      
+      printf("Count:%d\n\n",pairs->count);
+      pairs=pairs->next;
     }
-    }
+  }
+  return pairhead;
 }
