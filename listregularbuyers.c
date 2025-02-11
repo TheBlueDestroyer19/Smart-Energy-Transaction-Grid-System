e5e1a0277a5fdfba590df400e3c6a284217deecb
salilphanse <158063144+TheBlueDestroyer19@users.noreply.github.com>
2025-02-12 00:20:56 +0530

    Update listregularbuyers.c

--- listregularbuyers.c
+++ listregularbuyers.c
@@ -1,17 +1,47 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "template.h"
 
-void createListRegular(SellersList *sellerhead, seller_buyer *sellerlisthead) {
-  SellersList* temp=sellerhead;
-  seller_buyer* temp2=sellerlisthead;
-  int count=0;
+void createListRegular(SellersList *sellerhead, Pair *pairhead) {
+  Pair* ptr=pairhead;
+  SellersList *sptr;
+  IdNode* nptr;
+  int found;
+  if(ptr==NULL) printf("Create the list of buyer-seller pairs first!!\n");
+  while(ptr!=NULL && ptr->count>=5) {
+    sptr=sellerhead;
+    found=0;
+    while(sptr!=NULL && !found) {
+      if(ptr->sellerID==sptr->attributes.sellerID) {
+        found=1;
+        if(ptr->flag==0) {
+          ptr->flag=1;
+          nptr=(IdNode*)malloc(sizeof(IdNode));
+          nptr->id=ptr->buyerID;
+          nptr->next=NULL;
+          if(sptr->attributes.regularBuyersHead==NULL) sptr->attributes.regularBuyersHead=nptr;
+          else (sptr->attributes.regularBuyersTail)->next=nptr;
+          sptr->attributes.regularBuyersTail=nptr;
+        }
+      }
+      sptr=sptr->next;
+    }
+    ptr=ptr->next;
+  }
 
-  while(temp2!=NULL) {
-    TransNode *temp3=temp2->memberhead;
-    while(temp3!=NULL) {
-
+  if(pairhead!=NULL) {
+    printf("The list of regular buyers for different sellers:\n");
+    sptr=sellerhead;
+    while(sptr!=NULL) {
+      printf("\nSeller ID: %u\n",sptr->attributes.sellerID);
+      nptr=sptr->attributes.regularBuyersHead;
+      if(nptr==NULL) printf("No regular buyer");
+      while(nptr!=NULL) {
+        printf("%d, ",nptr->id);
+        nptr=nptr->next;
+      }
+      printf("\n");
+      sptr=sptr->next;
     }
   }
 }
