437480ae437e9bcc2b369c4bf5267fb3ccdeefc9
salilphanse <158063144+TheBlueDestroyer19@users.noreply.github.com>
2025-02-12 00:25:28 +0530

    Update updatedata.c

--- updatedata.c
+++ updatedata.c
@@ -3,36 +3,33 @@
 #include <stdlib.h>
 #include "template.h"
 
-void update(TransNode* mainhead)
-{
-    printf("Enter the transaction ID to update\n");
-    int id;
-    scanf("%d",&id);
-    TransNode* temp=mainhead;
-    int flag=0;
-    while(temp!=NULL&&flag==0)
-    {
-        if(temp->entry.transID==id)
+void update(TransNode* mainhead, SellersList* sellerhead) {
+  printf("Enter the transaction ID to update\n");
+  int id;
+  scanf("%d",&id);
+  TransNode* temp=mainhead;
+  SellersList* sptr=sellerhead;
+  int flag=0;
+
+  while(temp!=NULL&&flag==0) {
+    if(temp->entry.transID==id) flag=1;
+    else temp=temp->next;        
+  }
+
+  if(flag==1) {
+    printf("Enter the new amount of energy\n");
+    float amt;
+    scanf("%f",&amt);
+    temp->entry.amtOfEnergy=amt;
+    flag=0;
+    while(sptr!=NULL && !flag) {
+      if(sptr->attributes.sellerID == id) {
         flag=1;
-        else
-        temp=temp->next;
-        
+        if(temp->entry.amtOfEnergy>=300) temp->entry.pricePerKWh=sptr->attributes.a300;
+        else temp->entry.pricePerKWh=sptr->attributes.b300;
+      }
+      else sptr=sptr->next;
     }
-    if(flag==1)
-    {
-        printf("Enter the new amount of energy\n");
-        float amt;
-        scanf("%f",&amt);
-        printf("Enter the new Price Per kWh\n");
-        float price;
-        scanf("%f",&price);
-        temp->entry.amtOfEnergy=amt;
-        temp->entry.pricePerKWh=price;
-
-    }
-    else 
-          printf("No such transaction exists with the given Transaction ID\n");
-    
-    
-
+  }
+  else printf("No such transaction exists with the given Transaction ID\n");
 }
